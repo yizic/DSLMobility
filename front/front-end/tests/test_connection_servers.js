302e657bd6ab530a735164e99868b2b467e20834
BastienDidier <43957759+BastienDidier@users.noreply.github.com>
2020-03-26 10:16:39 +0100

    fix

--- front/front-end/tests/test_connection_servers.js
+++ front/front-end/tests/test_connection_servers.js
@@ -1,11 +1,10 @@
 var request = require("request");
 QUnit.test("test QUnit", function( assert ) {
-	var url = "http://127.0.0.1:8080/"
 	assert.ok(true, "QUnit OK")
 })
 QUnit.test("connection server", function( assert ) {
-	var url = "http://127.0.0.1:8080/test"
-	expect(0);
+	var url = "http://54.36.98.109:8080/test"
+	 var done = assert.async();
 	request.post(
 		url,
 		{
@@ -16,11 +15,13 @@ QUnit.test("connection server", function( assert ) {
 		(error, res, body) => {
 		if (error) {
 
-		  assert.ok(false,"connection server failed")
+		  assert.ok(false,"connection server failed");
+		  done();
 		  return
 		}
 		else{
-			assert.ok(true, "connection server ok");
+		  assert.ok(true, "connection server ok");
+		  done();
 		}
 
 	});
